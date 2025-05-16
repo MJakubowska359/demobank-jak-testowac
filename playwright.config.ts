ce1d64834dd8b27edf87702646f09683a31ccc5b
Monika Jakubowska <mjakubowska.official@gmail.com>
2025-05-16 23:35:35 +0200

    chore: playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -21,7 +21,10 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        baseURL: 'https://demobank.jaktestowac.pl',
+        browserName: 'chromium',
+      },
     },
   ],
 });
