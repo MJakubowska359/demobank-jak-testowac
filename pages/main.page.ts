90289f02acc7a565a46f0231c8290f8fe5c38a20
Monika Jakubowska <mjakubowska.official@gmail.com>
2025-05-24 22:29:25 +0200

    test(session): Add refresh session test by navigation between bookmarks [e2e]

---
+++ pages/main.page.ts
@@ -0,0 +1,17 @@
+import { BasePage } from './base.page';
+import { Page } from '@playwright/test';
+
+export class MainPage extends BasePage {
+  constructor(page: Page) {
+    super(page);
+  }
+
+  // Locators
+  extendSessionButton = this.page.getByRole('button', {
+    name: 'przedluż sesję',
+  });
+
+  // Locators for assertions
+  seconds = this.page.locator('#countdown_seconds');
+  minutes = this.page.locator('#countdown_minutes');
+}
---
+++ tests/session.spec.ts
@@ -0,0 +1,39 @@
+/* eslint-disable playwright/no-wait-for-timeout */
+import { LoginPage } from '../pages/login.page';
+import { MainPage } from '../pages/main.page';
+import { NavPage } from '../pages/nav.page';
+import { correctLogin } from '../test-data/login_data';
+import { expect, test } from '@playwright/test';
+
+test.describe('Check refresh session', () => {
+  let loginPage: LoginPage;
+  let mainPage: MainPage;
+  let navPage: NavPage;
+
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    mainPage = new MainPage(page);
+    navPage = new NavPage(page);
+
+    await page.goto('/');
+    await loginPage.login(correctLogin);
+  });
+
+  test(
+    'Should be able to refresh session between navigation by bookmarks',
+    { tag: '@smoke' },
+    async ({ page }) => {
+      // Arrange
+      const expectedSecondBeforeRefresh = '55';
+      const expectedMinutesAfterRefresh = '10';
+
+      // Act
+      await page.waitForTimeout(5_000);
+      await expect(mainPage.seconds).toHaveText(expectedSecondBeforeRefresh);
+      await navPage.clickTopUpPhoneButton();
+
+      // Assert
+      await expect(mainPage.minutes).toHaveText(expectedMinutesAfterRefresh);
+    },
+  );
+});
